b0fbd526536b94059babe68ec19d8e835282c5fc
saputraardhian <ardhiandwisaputra767@gmail.com>
2024-10-04 23:33:43 +0700

    Add files via upload

---
+++ 02_PENGENALAN_CPP_BAGIAN_2/TP2/TP-No2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void tukar(int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(){
+    int bil1, bil2;
+    cout << "Masukkan bilangan pertama: ";
+    cin >> bil1;
+    cout << "Masukkan bilangan kedua: ";
+    cin >> bil2;
+    cout << "Sebelum pertukaran:\n";
+    cout << "Bil1 : " << bil1 << "Bil2 : " << bil2 << endl;
+    tukar(& bil1, & bil2);
+    cout << "Setelah pertukaran:\n";
+    cout << "Bil1 : " << bil1 << "Bil2 : " << bil2 << endl;
+    return 0;
+
+
+}
